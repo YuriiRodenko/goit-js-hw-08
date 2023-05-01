de39f4b32efa10d8fa4a8efede7f911e511bf3a7
YuriiRodenko <jury.rodenko@gmail.com>
2023-05-01 11:46:37 +0300

    add task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('.feedback-form input');
+const inputMessage = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onFormInput() {
+  formData.email = inputEmail.value;
+  formData.message = inputMessage.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedSettings = localStorage.getItem(STORAGE_KEY);
+  if (savedSettings) {
+    const parsedSettings = JSON.parse(savedSettings);
+    inputEmail.value = parsedSettings.email;
+    inputMessage.value = parsedSettings.message;
+  }
+}
